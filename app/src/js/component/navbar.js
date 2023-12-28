a414ab97ba1c6e0e530eab5fce6355eb35df7f31
Zagarsix <claudk1006@gmail.com>
2023-12-28 01:10:46 -0300

    modified home and navbar

--- app/src/js/component/navbar.js
+++ app/src/js/component/navbar.js
@@ -6,13 +6,11 @@ import logo from "../../img/logo-4GeeksAcademy.png";
 export const Navbar = () => {
 	return (
 		
-		<div justify-content="space-between">
+		<div justify-content="space-between" style={{ backgroundColor: "grey" }}>
 			<Link className="navbar-brand me-5" to="/">
 				<img src={logo} alt="logo-4Geeks" width={200} height={100}></img>
 			</Link>
-<button>
-	
-</button>
+			<button type="button" className="btn btn-primary" to="/login">Login</button>
 
 		</div>
 	
--- app/src/js/views/home.js
+++ app/src/js/views/home.js
@@ -9,7 +9,7 @@ export const Home = () => {
 	return (
 	<>
 
-		<div className="text-center mt-5">
+		<div className="text-center mt-5" style={{ backgroundColor: "grey" }}>
 			<main className="main-section w-100 h-100 p-3 mb-5">
 				<h1>The Meetup Clone</h1>
 				<h3>This is a mini project created by <span className="text-primary"> 4GeeksAcademy</span></h3>
@@ -18,7 +18,7 @@ export const Home = () => {
 			</main>
 
 		</div>
-		<div>
+		<div className="text-center mt-5">
 		<div className="ml-auto">
 				<Link to="/events">
 					<p>5th Event for meetup 1</p>
