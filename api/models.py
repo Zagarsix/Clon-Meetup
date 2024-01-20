043cdcf0dfea056a67e7d7bdc47c8a7fb52877da
Zagarsix <claudk1006@gmail.com>
2024-01-19 23:53:19 -0300

    modified models and styles

--- api/models.py
+++ api/models.py
@@ -16,7 +16,7 @@ class User (db.Model):
     lastname = Column(String(100), nullable=False)
     email = Column(String(100), unique=True, nullable=False)
     password = Column(String(255), nullable=False)
-    is_active = Column(Boolean, default=True)
+    is_active = Column(Boolean(), default=True)
 
     def serialize(self):
         return {
@@ -39,60 +39,60 @@ class User (db.Model):
         db.session.delete(self)
         db.session.commit()
 
-# class Event (db.Model):
-#     __tablename__="events"
-#     id = Column(Integer, primary_key=True)
-#     tittle = Column(String(100), nullable=False)
-#     content = Column(String(255))
-#     day = Column(String(100), nullable=False)
-#     time = Column(String(100), nullable=False)
-#     Meetups = Column(String(100), unique=True, nullable=False)
-#     image = Column(String(255))
+class Event (db.Model):
+    __tablename__="events"
+    id = Column(Integer, primary_key=True)
+    tittle = Column(String(100), nullable=False)
+    content = Column(String(255))
+    day = Column(String(100), nullable=False)
+    time = Column(String(100), nullable=False)
+    Meetups = Column(String(100), unique=True, nullable=False)
+    image = Column(String(255))
 
-#     def serialize(self):
-#         return {
-#             'id': self.id,
-#             'tittle': self.tittle,
-#             'content': self.content,
-#             'day': self.day,
-#             'time': self.time,
-#             'meetups': self.meetups,
-#             'image': self.image,
-#         }
+    def serialize(self):
+        return {
+            'id': self.id,
+            'tittle': self.tittle,
+            'content': self.content,
+            'day': self.day,
+            'time': self.time,
+            'meetups': self.meetups,
+            'image': self.image,
+        }
 
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
     
-#     def update(self):
-#         db.session.commit()
+    def update(self):
+        db.session.commit()
 
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
 
-# class Meetup (db.Model):
-#     __tablename__="meetups"
-#     id = Column(Integer, primary_key=True)
-#     name = Column(String(100), unique=True, nullable=False)
-#     description = Column(String(255))
-#     image = Column(String(255))
+class Meetup (db.Model):
+    __tablename__="meetups"
+    id = Column(Integer, primary_key=True)
+    name = Column(String(100), unique=True, nullable=False)
+    description = Column(String(255))
+    image = Column(String(255))
 
-#     def serialize(self):
-#         return{
-#             'id': self.id,
-#             'name': self.name,
-#             'description': self.description,
-#             'image': self.image,
-#         } 
+    def serialize(self):
+        return{
+            'id': self.id,
+            'name': self.name,
+            'description': self.description,
+            'image': self.image,
+        } 
 
-#     def save(self):
-#         db.session.add(self)
-#         db.session.commit()
+    def save(self):
+        db.session.add(self)
+        db.session.commit()
     
-#     def update(self):
-#         db.session.commit()
+    def update(self):
+        db.session.commit()
 
-#     def delete(self):
-#         db.session.delete(self)
-#         db.session.commit()
+    def delete(self):
+        db.session.delete(self)
+        db.session.commit()
