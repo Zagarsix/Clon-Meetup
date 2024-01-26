6fadc4028107dca35ca2657b6f7926fc20d84b1c
Zagarsix <claudk1006@gmail.com>
2024-01-25 22:36:08 -0300

    adding routes and errors analysis

--- api/routes.py
+++ api/routes.py
@@ -13,6 +13,39 @@ def hello_world():
 
 @api.route("/register", methods=["POST"])
 def register():
+    try:
+        body = request.get_json()
+        if not "name" in body or not "lastname" in body or not "username" in body:      
+            return "Ha ocurrido un error"
+        if body["lastname"] == "":
+            return "Este campo es obligatorio"
+        
+        #instanciar una clase (crea un nuevo objeto):
+        user = User()
+        user.name = body["name"]
+        user.lastname = body["lastname"]
+        user.username = body["username"]
+        user.email = body["email"]
+        user.password = body["password"]
+        #le decimos que agregue a la base de datos la variable:
+        db.session.add(user)
+        db.session.commit()
+
+        print (body)
+        return jsonify(body)
+    except Exception as error:
+        return "Ha ocurrido un error en la base de datos"
+
+
+@api.route("/users", methods=["GET"])
+def users():
+    users = User.query.all() #Para solicitar todos los usuarios
+    serialize = list(map(lambda user: user.serialize(), users))
+    print(users)
+    return jsonify(serialize)
+
+@api.route("/login", methods=["POST"])
+def login():
     body = request.get_json()
     #instanciar una clase (crea un nuevo objeto):
     user = User()
@@ -27,13 +60,3 @@ def register():
 
     print (body)
     return jsonify(body)
-
-@api.route("/users", methods=["GET"])
-def users():
-    users = User.query.all() #Para solicitar todos los usuarios
-    serialize = list(map(lambda user: user.serialize(), users))
-    print(users)
-    return jsonify(serialize)
-
-
-
