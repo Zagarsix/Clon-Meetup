44b1449f405484b6a1fd2d06ec21b5b12a98a94f
Zagarsix <claudk1006@gmail.com>
2024-01-29 23:56:27 -0300

    creación de routes y control de errores

--- api/routes.py
+++ api/routes.py
@@ -15,9 +15,9 @@ def hello_world():
 def register():
     try:
         body = request.get_json()
-        if not "name" in body or not "lastname" in body or not "username" in body:      
+        if not "name" in body or not "lastname" in body or not "username" in body or not "email" in body or not "password" in body:    
             return "Ha ocurrido un error"
-        if body["lastname"] == "":
+        if body["name"] == "" or body["lastname"] == "" or body["username"] == "" or body["email"] == "" or body["password"] == "":
             return "Este campo es obligatorio"
         
         #instanciar una clase (crea un nuevo objeto):
@@ -34,6 +34,7 @@ def register():
         print (body)
         return jsonify(body)
     except Exception as error:
+        print(error)
         return "Ha ocurrido un error en la base de datos"
 
 
@@ -60,3 +61,22 @@ def login():
 
     print (body)
     return jsonify(body)
+
+@api.route("/events", methods=["POST"])
+def Event():
+    body = request.get_json()
+
+    event = events()
+    event.tittle = body["tittle"]
+    event.content = body["content"]
+    event.day = body["day"]
+    event.time = body["time"]
+    event.meetups = body["meetups"]
+    event.image = body["image"]
+
+    db.session.add(event)
+    db.session.commit()
+
+    print (body)
+    return jsonify(body)
+
